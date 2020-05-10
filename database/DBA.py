afe1cf882261fccc605909270e207df5e9237df2
Simon_Staehli <simon.staehli@students.fhnw.ch>
2020-05-10 13:18:15 +0200

    Class object for select statements

--- database/DBA.py
+++ database/DBA.py
@@ -1,32 +1,16 @@
 # -*- coding: utf-8 -*-
 
-"""class DBA(object):
-
-    #static variable dba (DBA-object) --> (_ before varname is to declare it as private)
-    _dba = None
-    _queries = {'key' : 'query'} #queries could be stored alternatively in a key:value file
-
-    def __init__(self):
-        #setup connection (con) here
-        self._con = con
-
-    @staticmethod
-    def get_DBA():
-        #implement singleton pattern
-        return dba
-
-    def query(self, key):
-        #perform query here
-        return result"""
-
-import psycopg2
-from psycopg2 import pool
+import sqlalchemy
 
 
 class DBA(object):
-    minimum_connections = 1
-    maximum_connections = 3
-    postgreSQL_pool = None
+    """
+    For the use of this class create a class object:
+    connection_object = DBA(database, user, password, host, port)
+    """
+
+    pool = False
+    pool_size = 1
 
     def __init__(self, database, user, password, host, port):
         self.database = database
@@ -34,81 +18,44 @@ class DBA(object):
         self.password = password
         self.host = host
         self.port = port
-        self.conn = None
-        self.cursor = None
+        self.engine = None
 
-    def initiate_connectionpool(self):
+    def _connect_to_db(self):
         """
-        Creates a connection pool if not already exists and adds a connection object to the class object
-        This connection pool only works for single-threaded processes. Allows the minimum amount of connections
-        and maximal amount of connections given.
-
-        Used package: https://pypi.org/project/psycopg2/
-
-        :param None
-        :returns Creates a connection pool and connects
+        Creates an SQLalchemy engine object with a connection pool with given pool size.
+        The engine operates as connector to the database which sends queries and receives data from the database.
         """
-        if DBA.postgreSQL_pool == None:
-            try:
-                DBA.postgreSQL_pool = psycopg2.pool.SimpleConnectionPool(DBA.minimum_connections,
-                                                                         DBA.maximum_connections,
-                                                                         user=self.user,
-                                                                         password=self.password,
-                                                                         host=self.host,
-                                                                         port=self.port,
-                                                                         database=self.database)
-                if (DBA.postgreSQL_pool):
-                    print("Connection pool created successfully")
-
-            except(Exception, psycopg2.DatabaseError) as error:
-                print("Error while connecting to PostgreSQL", error)
+        if DBA.pool == False:
+            self.engine = sqlalchemy.create_engine("postgresql://{}:{}@{}:{}/{}".format(self.user,
+                                                                                        self.password,
+                                                                                        self.host, self.port,
+                                                                                        self.database,
+                                                                                        pool_size=DBA.pool_size,
+                                                                                        max_overflow=0))
+            DBA.pool = True
         else:
-            print('Connection Pool already exists.')
+            pass
 
-    def query_data(self, statement):
+    def query_data(self, query):
         """
-        Sends a query to the database and returns the resulting records.
+        Gets a connection from the engine and performs given query.
+        It returns a pandas dataframe
 
-        Used package: https://pypi.org/project/psycopg2/
-
-        :param SQL-query
-        :returns records
+        :param SQL-Query
+        :returns pandas dataframe
         """
-        try:
-            if DBA.postgreSQL_pool:
-                # Use getconn() to Get Connection from connection pool
-                self.conn = DBA.postgreSQL_pool.getconn()
-            else:
-                raise Exception('Create the connection pool first with function initiate_connectionpool.')
+        if DBA.pool == False:
+            self._connect_to_db()
 
-            print("Connection to connection pool successfull. Make query ...")
-            self.cursor = self.conn.cursor()
+        with self.engine.connect() as connection:
+            result = pd.read_sql_query(query, con=connection)
 
-            # Perform statement on SQL-Server
-            self.cursor.execute(statement)
-            records = self.cursor.fetchall()
+        return result
 
-            self.cursor.close()
-
-        except (Exception, psycopg2.ProgrammingError) as error:
-            raise Exception('Something went wrong.')
-
-        finally:
-            # Returns the open connection to the connection pool
-            DBA.postgreSQL_pool.putconn(self.conn)
-            self.conn = None
-            print('Connection returned to pool.')
-
-        return records
-
-    def close_connection_pool(self):
+    def close_connection(self):
         """
-        Turns off all Connections to the server from the pool object.
+        Closes all unused connections of the connection engine of SQL-Alchemy
         """
-        # use closeall method to close all the active connections
-        if (DBA.postgreSQL_pool):
-            DBA.postgreSQL_pool.closeall
-            DBA.postgreSQL_pool = None
-            print("All connections closed of Connection Pool")
-        else:
-            print('All connections already closed.')
+        self.engine.dispose()
+        self.engine = None
+        DBA.pool = False
